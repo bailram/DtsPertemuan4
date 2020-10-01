4b44a7fff75366797e2237de15d1a21737665fbb
Dhimas Bayu Ilham Ramadhan <dhimasbayu.9f@gmail.com>
2020-10-01 21:22:22 +0700

    add task4

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 task1p1 = document.getElementById("task1p1");
 task1p2 = document.getElementById("task1p2");
-task3Arial = document.getElementById("ex1");
-task3Verdana = document.getElementById("ex2");
-task3Times = document.getElementById("ex3");
+task3Arial = document.getElementById("task3p1");
+task3Verdana = document.getElementById("task3p2");
+task3Times = document.getElementById("task3p3");
+task4p1 = document.getElementById("task4p1");
+task4p2 = document.getElementById("task4p2");
+task4p3 = document.getElementById("task4p3");
 
 // latihan 1
 task1p1.innerHTML = "Selamat Belajar HTML";
@@ -12,3 +15,8 @@ task1p2.innerHTML = "Semoga Sukses!";
 task3Arial.innerHTML = "Arial, contoh AC Milan";
 task3Verdana.innerHTML = "Verdana, contoh Persebaya";
 task3Times.innerHTML = "Times New Roman, contoh Indonesia";
+
+// latihan 4
+task4p1.innerHTML = "PSM Makassar";
+task4p2.innerHTML = "Ini tetap PSM Makassar kan?";
+task4p3.innerHTML = "Ini baru Persebaya";
