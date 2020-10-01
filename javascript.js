de792b223320e14f9eb3b8d914d1536ae7e0b785
Dhimas Bayu Ilham Ramadhan <dhimasbayu.9f@gmail.com>
2020-10-01 20:50:35 +0700

    add training task1 and task2

---
+++ javascript.js
@@ -0,0 +1,12 @@
+task1p1 = document.getElementById("task1p1");
+task1p2 = document.getElementById("task1p2");
+task2Header = document.getElementById("headerTask2");
+task2Content = document.getElementById("contentTask2");
+
+// latihan 1
+task1p1.innerHTML = "Selamat Belajar HTML";
+task1p2.innerHTML = "Semoga Sukses!";
+
+// latihan 2
+task2Header.innerHTML = "<h3>Aneka Tanaman Hias</h3>";
+task2Content.innerHTML = "<hr>Tes"
