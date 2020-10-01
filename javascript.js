3824ebbd78232743112570d800ac5715e6d2eba4
Dhimas Bayu Ilham Ramadhan <dhimasbayu.9f@gmail.com>
2020-10-01 21:16:03 +0700

    removing js task 2 and adding task 3

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 task1p1 = document.getElementById("task1p1");
 task1p2 = document.getElementById("task1p2");
-task2Header = document.getElementById("headerTask2");
-task2Content = document.getElementById("contentTask2");
+task3Arial = document.getElementById("ex1");
+task3Verdana = document.getElementById("ex2");
+task3Times = document.getElementById("ex3");
 
 // latihan 1
 task1p1.innerHTML = "Selamat Belajar HTML";
 task1p2.innerHTML = "Semoga Sukses!";
 
-// latihan 2
-task2Header.innerHTML = "<h3>Aneka Tanaman Hias</h3>";
-task2Content.innerHTML = "<hr>Tes"
+// latihan 3
+task3Arial.innerHTML = "Arial, contoh AC Milan";
+task3Verdana.innerHTML = "Verdana, contoh Persebaya";
+task3Times.innerHTML = "Times New Roman, contoh Indonesia";
